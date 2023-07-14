a634629069f5f4cf6a366d3ae200cb58e7d10aa8
AugustoHoffmeister <augustohof@yahoo.com>
2023-07-14 12:16:20 -0300

    Update index.ts

--- -TS/aula02/src/index.ts
+++ -TS/aula02/src/index.ts
@@ -1,7 +1,30 @@
-// Comparação
-let a:number = 1
+// Tipos Básicos, Simples e Primitivos de dados
 
-console.log('Valor de a: ',a)
-console.log('Valor de a++: ',a++)
-console.log('Valor de ++a: ',++a)
-console.log('Valor de final de a: ',a)
+// string - valores de texto
+let nome: string = "Augusto";
+
+// number - números inteiros e valores de ponto flutuante
+let idade: number = 43;
+
+// boolean - valores verdadeiros (true) ou falsos (false)
+let ativo: boolean = true;
+
+// undefined (indefinido)
+let bizarro: undefined = undefined;
+
+// null (nulo)
+let nada: null = null;
+
+/* Há também 2 tipos primitivos menos comuns, encontrados em versões mais recentes de JS e TS - bigint e symbol */
+
+/** Ao criar uma varíavel, há duas maneiras principais de o TS atribuir um tipo:
+ * Explicito
+ * Implicito
+ */
+/** Tipo Explicito - quando você atribui o tipo de dado na declaração. */
+let sobrenome: string = "Hoffmeister";
+// sobrenome = 30; // TS aponta erro
+
+/** Tipo Implicito - quando o TS ira "adivinhar" inferir o tipo com base no valor atribuido */
+let profissao = "programador";
+// profissao = 100; TS aponta erro
